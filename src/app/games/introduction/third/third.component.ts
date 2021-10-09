e7526326633c91afc64945595b889b742fc01e89
jacolpn <jacolpn@outlook.com>
2021-10-08 23:03:52 -0300

    added styles in introduction/third level

--- src/app/games/introduction/third/third.component.ts
+++ src/app/games/introduction/third/third.component.ts
@@ -8,26 +8,41 @@ import { Component, OnInit } from '@angular/core';
 export class ThirdComponent implements OnInit {
   openModal: boolean = false;
   modaldialog: boolean = false;
-  firstLine: string = "export class IntroductionComponent implements OnInit {";
-  thirdLine: string = "constructor() { }";
-  fourthLine: string = "ngOnInit(): void { }";
-  fifthLine: string = "}";
+  oneLineTS: string = "export class IntroductionComponent implements OnInit {";
+  twoLineTS: string = "texto: string = \"Olá Mundo\";";
+  threeLineTS: string = "";
+  fourLineTS: string = "constructor() { }";
+  fiveLineTS: string = "";
+  sixLineTS: string = "ngOnInit(): void { }";
+  sevenLineTS: string = "}";
+  eightLineTS: string = "";
 
-  firstLineHtml: string = "<section>";
-  secondLineHtml: string = "{{ texto }}";
-  thirdLineHtml: string = "<input [placeholder]=\"texto\">";
-  fourthLineHtml: string ="<input";
-  fifthLineHtml: string = "</section>";
-  textInputHtml: string = "";
-  textCorrectHtml: string = "[placeholder]=\"texto\"";
+  oneLineHTML: string = "<section>";
+  twoLineHTML: string = "{{ texto }}";
+  threeLineHTML: string = "";
+  fourLineHTML: string ="<input";
+  fiveLineHTML: string = "[placeholder]=\"texto\"";
+  sixLineHTML: string = "=\"texto == 'Olá Mundo'\"";
+  sixCorrectLineHTML: string = "[class.success]=\"texto == 'Olá Mundo'\"";
+  sevenLineHTML: string = ">";
+  eightLineHTML: string = "";
+  nineLineHTML: string = "</section>";
+  tenLineHTML: string = "";
 
+  textInputHTML: string = "";
+  textCorrectHTML: string = "[class.success]";
+
+  oneLineCSS: string = ".success {"
+  twoLineCSS: string = "border: 2px solid rgba(53, 201, 75);"
+  threeLineCSS: string = "}"
+  fourLineCSS: string = ""
 
   constructor() { }
 
   ngOnInit(): void { }
 
   ngAfterViewInit(): void {
-    if (this.textInputHtml == this.textCorrectHtml) {
+    if (this.textInputHTML == this.textCorrectHTML) {
       this.modaldialog = !this.modaldialog;
     }
   }
@@ -37,7 +52,7 @@ export class ThirdComponent implements OnInit {
   }
 
   conclude() {
-    if (this.textInputHtml == this.textCorrectHtml) {
+    if (this.textInputHTML == this.textCorrectHTML) {
       this.modaldialog = !this.modaldialog;
     }
   }
